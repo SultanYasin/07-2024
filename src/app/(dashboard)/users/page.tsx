71f8db0e61ab8751a4759c71de47b57455bfdce0
Sultan Yasin <sultan.yasin@utb.ecutbildning.se>
2024-08-02 10:00:47 +0200

    add filtering to tables

--- src/app/(dashboard)/users/page.tsx
+++ src/app/(dashboard)/users/page.tsx
@@ -1,33 +1,36 @@
 "use client";
 import React from "react";
-import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
+import { DataGrid, GridRowsProp, GridColDef, GridToolbar } from "@mui/x-data-grid";
 import { Box, Typography } from "@mui/material";
 import { useTheme } from "@emotion/react";
 import { rows } from "./MockingData";
 import AdminPanelSettingsTwoToneIcon from "@mui/icons-material/AdminPanelSettingsTwoTone";
-import SupervisorAccountTwoToneIcon from '@mui/icons-material/SupervisorAccountTwoTone';
+import SupervisorAccountTwoToneIcon from "@mui/icons-material/SupervisorAccountTwoTone";
 import LockPersonTwoToneIcon from "@mui/icons-material/LockPersonTwoTone";
 
 export default function Users() {
   const theme = useTheme();
 
   const columns: GridColDef[] = [
-    { field: "id", headerName: "ID", width: 80 },
+    { field: "id", headerName: "ID", width: 70 },
     { field: "name", headerName: "NAME", flex: 1 },
     { field: "email", headerName: "EMAIL", flex: 1 },
     { field: "department", headerName: "DEPARTMENT", flex: 1 },
     { field: "job_title", headerName: "TITLE", flex: 1 },
     {
-      field: "col6",
+      field: "access",
       headerName: "ACCESS LEVEL",
-      flex: 1,
+      width: 190,
       renderCell: ({ row: { access } }) => {
         return (
           <Box
             sx={{
-              p: "1px",
+              pt: "1px",
+              mt: "10px",
               textAlign: "center",
               display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
               backgroundColor:
                 access === "admin"
                   ? "gray"
@@ -40,18 +43,22 @@ export default function Users() {
             {access === "manager" && <SupervisorAccountTwoToneIcon />}
             {access === "user" && <LockPersonTwoToneIcon />}
 
-            <Typography pl={2}> {access} </Typography>
+            <Typography pl={2} style={{ fontSize: "18px" }}>
+              {access}
+            </Typography>
           </Box>
         );
       },
     },
   ];
 
-
   return (
-<div style={{ height: 750, width: "100%" }}>
-        <DataGrid rows={rows} columns={columns} />
-      </div>
-  
+    <Box style={{ height: "85vh", width: "100%", mx: "auto" }}>
+      <DataGrid
+        slots={{ toolbar: GridToolbar,}}
+        rows={rows}
+        columns={columns}
+      />
+    </Box>
   );
 }
